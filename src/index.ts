5382784236fa6d88d837b2fa1f673ab615852e88
regod <phvietan@gmail.com>
2020-09-02 05:36:28 +0000

    after struggle with csp and cdn, remove fontawesome

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,68 @@
 import path from "path";
-import helmet from "helmet";
+import crypto from "crypto";
 import dotenv from "dotenv";
 import express from "express";
 import nunjucks from "nunjucks";
 
+import { Request, Response, NextFunction, Express } from 'express';
+
 dotenv.config();
 
-const app = express();
+function prepareTemplateEngine(app: Express) {
+    const env = nunjucks.configure(
+        path.join("src", "views"), {
+        autoescape: true,
+        express: app,
+    });
+    env.addGlobal("HTML_TITLE", process.env.HTML_TITLE);
+    env.addGlobal("HTML_DESCRIPTION", process.env.HTML_DESCRIPTION);
+}
 
-app.use(helmet());
-app.use(helmet.frameguard({ action: 'deny' }));
+function removeAllChars(s: string, c: string):string {
+    return s.split(c).join('');
+}
 
-const env = nunjucks.configure(
-    path.join("src", "views"), {
-    autoescape: true,
-    express: app,
-});
+function prepareMiddlewares(app: Express) {
+    app.use((_, res: Response, next: NextFunction) => {
+        res.locals.cspNonce = crypto.randomBytes(16).toString("hex");
+        const csp = `
+            default-src 'none';
+            base-uri 'none';
+            block-all-mixed-content;
+            font-src 'self' https: data:;
+            frame-ancestors 'self';
+            img-src 'self' data:;
+            object-src 'none';
+            script-src 'nonce-${res.locals.cspNonce}';
+            style-src 'nonce-${res.locals.cspNonce}';
+            upgrade-insecure-requests;
+        `;
+        res.set("Content-Security-Policy",
+            removeAllChars(removeAllChars(csp, '\n'), '  ')
+        );
+        return next();
+    });
+}
 
-env.addGlobal("HTML_TITLE", process.env.HTML_TITLE);
-env.addGlobal("HTML_DESCRIPTION", process.env.HTML_DESCRIPTION);
+function prepareEndpoints(app: Express) {
+    app.get( "/", ( req: Request, res: Response ) => {
+        const context = {
+            nonce: res.locals.cspNonce,
+        };
+        res.render("index.html", context);
+    } );
+}
 
-app.get( "/", ( req, res ) => {
-    res.render("index.html");
-} );
+function main() {
+    const app = express();
+    prepareTemplateEngine(app);
+    prepareMiddlewares(app);
+    prepareEndpoints(app);
 
-const port = process.env.APP_PORT || 8000;
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+    const port = process.env.APP_PORT || 8000;
+    app.listen( port, () => {
+        console.log( `server started at http://localhost:${ port }` );
+    } );
+}
+
+main();
